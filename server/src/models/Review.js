0a389c94d33a39fbf816142924d77a46f7c0051a
0rph3n <113313679+0rph3n@users.noreply.github.com>
2023-11-21 12:07:39 -0300

    Feature Implementación Redux (#3)
    * CreateFolders (#1)
    
    Creacion de carpetas de front
    
    * creados los modelos (#2)
    
    Creados los modelos, de users, products, reviews y orders
    
    * Se implemento redux en el proyecto, se crearon la estrucutra de carpetas y se realizo la configuración inicial de actions, reducer, store y Provider en el archivo main.jsx
    
    ---------
    
    Co-authored-by: Mateo Marengo <127827134+MOMarengo@users.noreply.github.com>
    Co-authored-by: Lucas Derka <112895046+lucasderka3@users.noreply.github.com>

---
+++ server/src/models/Review.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Review', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      unique: true
+    },
+    content: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    score: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate:{
+            min: 1,
+            max: 5
+        }
+    }
+  });
+};
