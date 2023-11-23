371ba49a6a135eaa0d90583a0b88669fff6fcba1
Diego  Mendoza <diegoxesfc@gmail.com>
2023-11-22 22:41:03 -0500

    Tercer_avance_Reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,9 +1,27 @@
 import {} from "../actions/index";
 
-let initialState = {};
+let initialState = {
+    productsDetail:null,
+
+
+};
 
 function rootReducer(state = initialState, action) {
   switch (action.payload) {
+    case 'GET_PRODUCT_DETAIL':
+      return {
+        ...state,
+        pokemonDetail: action.payload,
+      };
+    case 'CLEAN_PRODUCT_DETAIL':
+      return {
+        ...state,
+        pokemonDetail: null,
+      };
+
+
+
+
     default:
       return state;
   }
