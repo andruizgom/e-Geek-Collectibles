a44cf8eaa54e9e7cb2ad284b07410651a5003d03
0rph3n <113313679+0rph3n@users.noreply.github.com>
2023-11-23 19:19:35 -0300

    Leandro Farnochi (#18)
    * Feature Implementación Redux (#3)
    
    * CreateFolders (#1)
    
    Creacion de carpetas de front
    
    * creados los modelos (#2)
    
    Creados los modelos, de users, products, reviews y orders
    
    * Se implemento redux en el proyecto, se crearon la estrucutra de carpetas y se realizo la configuración inicial de actions, reducer, store y Provider en el archivo main.jsx
    
    ---------
    
    Co-authored-by: Mateo Marengo <127827134+MOMarengo@users.noreply.github.com>
    Co-authored-by: Lucas Derka <112895046+lucasderka3@users.noreply.github.com>
    
    * Se implementa la Home, se traen los productos y se muestran de una manera rustica
    
    * Se corrige el link de las imagenes en la base de datos
    
    * Se aplican los estilos a card y cards
    
    * Se instala dependecia de infinite scroll
    
    * Se finalizan los estilos de las cards
    
    ---------
    
    Co-authored-by: Mateo Marengo <127827134+MOMarengo@users.noreply.github.com>
    Co-authored-by: Lucas Derka <112895046+lucasderka3@users.noreply.github.com>

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Card.styles.css";
 
 function Card({ title, id, img, price }) {
   return (
-    <div>
-      <Link to={`/detail/${id}`}>
+    <div className="card">
+      <Link to={`/detail/${id}`} className="contentBox">
         <h2>{title}</h2>
-        <img src={img} />
-        <p>{price}</p>
+        <div className="imgBox">
+          <img src={img} className="mouse" />
+        </div>
+        <p>${price}</p>
+        <a href="#" className="buy">
+          Comprar
+        </a>
       </Link>
     </div>
   );
