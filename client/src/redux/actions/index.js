416a3ae5ba6532c9ed3d9393af8abdbb9e6fe6d1
Diego  Mendoza <diegoxesfc@gmail.com>
2023-11-22 19:42:09 -0500

    Avanze2_Actions_Detail

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,3 +1,27 @@
 import axios from "axios";
 
+import {
+    GET_PRODUCT_DETAIL,
+    CLEAN_PRODUCT_DETAIL
+} from "./types"
 
+
+export const getProductDetail = (id) => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios(`${URL}/${id}`);
+            await dispatch({
+                type: GET_PRODUCT_DETAIL,
+                payload: data
+            });
+        } catch (error) {
+            console.log(error.message)
+        } 
+    };
+};
+
+export const cleanProductDetail = () => { //reestablece el estado
+    return {
+        type: CLEAN_PRODUCT_DETAIL
+    };
+};
