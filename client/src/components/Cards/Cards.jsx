cd4cbf3fc3b5e78f4ba7b8355289e676050074ae
0rph3n <leandrofarnochi@hotmail.com>
2023-11-23 19:07:10 -0300

    Se instala dependecia de infinite scroll

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Card from "../Card/Card";
 import PageNotFound from "../PageNotFound/PageNotFound";
 
+import "./Cards.styles.css";
+
 function Cards({ allProducts }) {
   return (
-    <div>
+    <div className="container">
       {allProducts ? (
         allProducts.map((prod) => {
           return (
