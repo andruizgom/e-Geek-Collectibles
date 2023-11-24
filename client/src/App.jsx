2295a747a67ec731dd59437656c913d21a6f501e
ju4nqleyton <ju4nqleyton@gmail.com>
2023-11-24 14:50:52 -0500

    fix landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,13 @@ import Home from './views/Home/Home';
 import Detail from './views/Detail/Detail';
 import './App.css';
 
-import NavBar from './components/NavBar/NavBar';
-
 function App() {
-
-
   return (
     <div className="App">
-      <NavBar/>
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
-        
-        
-        
       </Routes>
     </div>
   );
