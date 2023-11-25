0977884c9d0e415e9aadc1131e8f6e92eeda6f01
0rph3n <leandrofarnochi@hotmail.com>
2023-11-25 13:21:50 -0300

    Se aplican detalles en el detail que mejoran la comprension de las propiedades del producto, se aplica un cleandetail para borrar el producto al salir del mismo

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import {useParams} from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { getProductById, resetProductDetail } from "../../redux/actions";
+import { Link } from 'react-router-dom';
 
-const Detail = () => {
-  const { id } = useParams();
+import "./detail.css"
 
-  return (
-    <div>
-      <h1>Detail: {id}</h1>
-    </div>
-  );
-};
 
-export default Detail;
+export default function Detail(){
+  const useProducts= () => {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const productsDetail = useSelector((state) => state.productsDetail);
+    
+    useEffect(() => {
+      if (id) {
+        dispatch(getProductById(id, false))
+          .then(() => console.log("Éxito"))
+          .catch((err) => {
+            throw new Error("Error en la acción:", err);
+          });
+      }
+      return () => {
+        dispatch(resetProductDetail());
+      };
+    }, [dispatch, id]);
+
+    return productsDetail  
+  }
+  
+
+  const productDetail=useProducts();
+
+  return(
+      <div className="containerDetail">
+          <Link to="/Home"><button className="btn-back">🔙</button></Link>
+          <img src={productDetail.image} className="image"/>
+          <div className="titles">
+          <h2>{productDetail.title}</h2>
+          <h3>Fabricante: {productDetail.manufacturer}</h3>
+          <h3>Creador: {productDetail.author}</h3>
+          <h3>Stock: {productDetail.stock} unidades</h3>
+          <h3>${productDetail.price}</h3>
+          <h3>Categoria: {productDetail.category}</h3>
+          <h4>{productDetail.description}</h4>
+          </div>
+      </div>
+  )
+}
+
+
