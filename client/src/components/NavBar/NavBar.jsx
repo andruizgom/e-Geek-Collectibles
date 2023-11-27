0d26c3f7d5fa383727d5fac86c7a383618fc7d6f
0rph3n <113313679+0rph3n@users.noreply.github.com>
2023-11-27 14:00:46 -0300

    Leandro farnochi - SearchBar y Navbar (#29)
    * Se implementa la Home, se traen los productos y se muestran de una manera rustica
    
    * Se corrige el link de las imagenes en la base de datos
    
    * Se implementan estilos en el componente cards
    
    * Se aplican los estilos a card y cards
    
    * Se instala dependecia de infinite scroll
    
    * Se finalizan los estilos de las cards
    
    * Se actualizan cambios
    
    * Se aplican detalles en el detail que mejoran la comprension de las propiedades del producto, se aplica un cleandetail para borrar el producto al salir del mismo
    
    * Se implementan las review en el detail y se corrigen estilos
    
    * Se corrige formato de searchbar y doble renderizado de detail
    
    * Se mejora el funcionamiento de la searchbar, ahora muestra en el desplegable el error y ademas cuando se ingresan 3 caracteres indica si hay un error. Ademas se elimina un import duplicado en la Cards component
    
    * Se completa el funcionamiento de la searchbar permiento que rediriga al detail del producto seleccionado
    
    * Se implementa el link al formulario Create Product
    
    * Se limpio el código de la searchbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import SearchBar from '../SearchBar/SearchBar'; // Asegúrate de que la ruta es correcta
+import SearchBar from '../SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
       <h2 className={styles.title}>Collectibles</h2>
+      <Link to="/create">Create Product</Link>
       <SearchBar />
-      {/* mas elementos? */}
     </nav>
   );
 }
