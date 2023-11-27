79d1f8460d9d223c8dec651e4466bbf659b0b152
LDerka <derkal15@gmail.com>
2023-11-27 14:01:16 -0300

    creacion de filtros en el back y front

---
+++ server/src/controllers/Filters/allFilters.js
@@ -0,0 +1,55 @@
+const {Op} = require('sequelize')
+const { Products } = require('../../db');
+
+const filterProducts = async(req) => {
+    const { title, price, category, sortOrder, nameOrder} = req.query;
+    try {
+        
+        const filterOptions = {};
+        
+        if(title){
+            filterOptions.title = {
+                [Op.iLike]: `%${title}%`
+            }
+        }
+
+        if(price){
+            filterOptions.price = {
+                [Op.lte]: parseFloat(price)
+            };
+        }
+
+        if(category){
+            filterOptions.category = {
+                [Op.iLike]: `%${category}%`
+            };
+        }
+
+        const orderOptions = [];
+
+        if(sortOrder === 'priceDesc'){
+            orderOptions.push(['price', 'DESC']);
+        } else if( sortOrder === 'priceAsc'){
+            orderOptions.push(['price', 'ASC']);
+        }
+
+        if (nameOrder === 'nameDesc') {
+            orderOptions.push(['title', 'DESC']);
+        } else if (nameOrder === 'nameAsc') {
+            orderOptions.push(['title', 'ASC']);
+        }
+
+        const filteredProducts = await Products.findAll({
+            where: filterOptions,
+            order: orderOptions
+        })
+
+        return filteredProducts;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+module.exports = {
+    filterProducts
+}
