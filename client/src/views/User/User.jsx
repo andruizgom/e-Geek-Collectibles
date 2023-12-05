b7f4e1434bbffc551ef7c6ff9ab1118f21ed03a4
franquesino <you@example.com>
2023-12-05 09:21:11 -0300

    funcionando

--- client/src/views/User/User.jsx
+++ client/src/views/User/User.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import Favorites from '../../components/Favorites/Favorites'
+import React, { useState } from "react";
+import Favorites from '../../components/Favorites/Favorites';
+import Review from '../../components/Review/Review';
 
 function User() {
-  return <div>
-    <Favorites/>
-    </div>;
+  const [isReviewFormOpen, setIsReviewFormOpen] = useState(false);
+  const [selectedProductId, setSelectedProductId] = useState(null);
+
+  const handleOpenReviewForm = (productId) => {
+    setIsReviewFormOpen(true);
+    setSelectedProductId(productId);
+  };
+
+  const handleCloseReviewForm = () => {
+    setIsReviewFormOpen(false);
+    setSelectedProductId(null);
+  };
+
+  return (
+    <div>
+      <Favorites onReviewClick={handleOpenReviewForm} />
+      {isReviewFormOpen && (
+        <div>
+          <Review productId={selectedProductId} onClose={handleCloseReviewForm} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default User;
