85562df9281ca445d82e028f0b5de5addbfbbea5
MOMarengo <momarengo@gmail.com>
2023-12-05 19:35:26 -0300

    Stock Unidades

--- client/src/components/Stripe/PaymentForm.jsx
+++ client/src/components/Stripe/PaymentForm.jsx
@@ -3,8 +3,9 @@ import { loadStripe } from "@stripe/stripe-js";
 
 const stripePromise = loadStripe('pk_test_51OHSFxEdGwHq7UR2MSY16IkLw9ATiMPpMbDz4o3pQKINyv0gNmxMnW8YB1me0V7pfzRGrkEgjPfeOvrstgT6jWId00FqILQQ0n')
 
-const PaymentForm = ({productId}) => {
+const PaymentForm = ({ productId, quantity }) => { 
     const [loading, setLoading] = useState(false);
+
     const handlePayment = async () => {
         setLoading(true);
 
@@ -14,23 +15,23 @@ const PaymentForm = ({productId}) => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({productId})
-            })
-            
+                body: JSON.stringify({ productId, quantity }), 
+            });
+
             const session = await response.json();
-            
+
             const stripe = await stripePromise;
             const result = await stripe.redirectToCheckout({
                 sessionId: session.id
-            })
+            });
 
-            if(result.error){
+            if (result.error) {
                 console.log(result.error.message);
                 setLoading(false)
             }
         } catch (error) {
-            console.error(error)
-            setLoading(false)            
+            console.error(error);
+            setLoading(false);
         }
     }
 
@@ -39,9 +40,8 @@ const PaymentForm = ({productId}) => {
             <button onClick={handlePayment} disabled={loading} className="flex w-full items-center justify-center rounded-md border border-transparent bg-green-500 px-8 py-3 text-base font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2">
                 {loading ? 'Procesando...' : 'Buy'}
             </button>
-        </div>    
+        </div>
     )
 }
-
 
 export default PaymentForm;
